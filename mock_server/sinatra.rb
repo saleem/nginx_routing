faf45529fefb668609a692c93355701e2415361c
Saleem Siddiqui <Siddiqui.Saleem@gmail.com>
2020-01-15 13:29:42 -0600

    simplified sinatra multiplexer

--- mock_server/sinatra.rb
+++ mock_server/sinatra.rb
@@ -1,16 +1,6 @@
 require 'sinatra'
 
-get '/guideme' do
-  redirect 'http://www.google.com'
-end
-
-get '/routeme' do
+get '/*' do
   redirect request.env["HTTP_X_MYTOKEN"]
 end
 
-get '/:name' do
-  'Hallo, Welt!'
-  "You accessed the url \"#{params['name']}\""
-  "Some of your headers are below:"
-  request.env["HTTP_X_MYTOKEN"]
-end
